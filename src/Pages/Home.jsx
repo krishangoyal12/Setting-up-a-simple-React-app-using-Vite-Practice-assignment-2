8401f35c7bedb328a4b80d709fcb0f8ae71f61e9
krishangoyal12 <krishangoyal717@gmail.com>
2025-02-28 09:31:55 +0530

    Completed the assignment

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Krishan Goyal</h1>
+      <h2>Contact me: <a href="mailto:krishan.goyal.s84@kalvium.community">krishan.goyal.s84@kalvium.community</a></h2>
+
+    </>
+  )
+}
